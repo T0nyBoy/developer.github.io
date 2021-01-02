959f654e2c2188e713b0fd59266d3ee4f48cea4e
T0nyBoy <T0nyBoy@users.noreply.github.com>
2021-01-02 13:06:10 +0000

    updates

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,6 +1,6 @@
 const mySiema = new Siema(
   {
-      selector: '.siema',
+      selector: '.slide1',
       duration: 200,
       easing: 'ease-out',
       perPage: 1,
@@ -14,7 +14,44 @@ const mySiema = new Siema(
       onChange: () => {},
     }
 );
-
 setInterval(() => mySiema.next(), 3000)
-document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+const mySiema2 = new Siema(
+  {
+      selector: '.slide2',
+      duration: 200,
+      easing: 'ease-out',
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: true,
+      rtl: false,
+      onInit: () => {},
+      onChange: () => {},
+    }
+);
+setInterval(() => mySiema2.next(), 3000)
+
+const mySiema3 = new Siema(
+  {
+      selector: '.slide3',
+      duration: 200,
+      easing: 'ease-out',
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: true,
+      rtl: false,
+      onInit: () => {},
+      onChange: () => {},
+    }
+);
+setInterval(() => mySiema3.next(), 3000)
+
+
+// document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+// document.querySelector('.next').addEventListener('click', () => mySiema.next());
